f73208c01ff7599c62b553d21b2c28cde1776cdb
Sai Krishna <sana@Sais-MacBook-Air.local>
2026-05-14 09:33:01 +0530

    Add CORS headers for Netlify frontend

--- app.py
+++ app.py
@@ -4,6 +4,13 @@ import time
 
 app = Flask(__name__)
 
+@app.after_request
+def add_cors(response):
+    response.headers["Access-Control-Allow-Origin"] = "*"
+    response.headers["Access-Control-Allow-Methods"] = "GET, OPTIONS"
+    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
+    return response
+
 # Simple in-memory cache: {brand: {products: [...], fetched_at: timestamp}}
 _cache = {}
 _cache_ttl = 1800  # 30 minutes
